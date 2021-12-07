481e4c6c45f62892f939333aa33aafdd4cf84fdb
Darko Kukovec <darko@kukovec.eu>
2021-12-07 07:05:21 +0100

    Day 6

---
+++ 6/a.js
@@ -0,0 +1,24 @@
+const fish = require('./input').split(',').map(Number);
+
+const DAYS = 80;
+let count = 0;
+const cache = {};
+
+function getCount(f) {
+    if (cache[f]) {
+        return cache[f];
+    }
+    let count = 1;
+    for (let x = f; x < DAYS; x += 7) {
+        count += getCount(x + 9);
+    }
+    cache[f] = count;
+    return count;
+}
+
+while(fish.length) {
+    const f = fish.shift();
+    count += getCount(f);
+}
+
+console.log(count);
